e6b240a998e3a0680e3d8572ec408ef1cc5ded54
Duy Ba <duy.nguyen@whydah.xyz>
2024-03-28 21:11:38 +0700

    .

--- taproot/model/mint/request.go
+++ taproot/model/mint/request.go
@@ -6,7 +6,7 @@ import (
 )
 
 type MintAssetReq struct {
-	AmountSats        int32              `json:"amount_sats"`
-	TapScriptRootHash *chainhash.Hash    `json:"tap_script_root_hash"`
-	MintProof         *proof.AssetProofs `json:"mint_proof"`
+	AmountSats        int32             `json:"amount_sats"`
+	TapScriptRootHash *chainhash.Hash   `json:"tap_script_root_hash"`
+	MintProof         proof.AssetProofs `json:"mint_proof"`
 }
